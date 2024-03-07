e1e74b395b47a74bb9ae113fc7b117c514ae49d1
Lukas MAUFFRÉ <lukasapplefan@gmail.com>
2024-03-07 19:04:47 +0100

    Add Apple Watch app

---
+++ CompteurWatch Watch App/CompteurWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompteurWatchApp.swift
+//  CompteurWatch Watch App
+//
+//  Created by Lukas Mauffré on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CompteurWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
