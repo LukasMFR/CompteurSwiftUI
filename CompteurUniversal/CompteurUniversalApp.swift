1ea3798bed8b1916ca0bfa93f2ccf270c207c206
Lukas Mauffré <lukas.mauffre@icloud.com>
2024-03-07 15:12:52 +0100

    Initial Commit

---
+++ CompteurUniversal/CompteurUniversalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompteurUniversalApp.swift
+//  CompteurUniversal
+//
+//  Created by Lukas Mauffré on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CompteurUniversalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
